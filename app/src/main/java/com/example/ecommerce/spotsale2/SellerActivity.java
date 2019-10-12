1395222e362430257b71595032467ff7cd8a25b2
athang213 <athang213@gmail.com>
2019-10-12 22:51:51 +0530

    Minor Bug Fix

--- app/src/main/java/com/example/ecommerce/spotsale2/SellerActivity.java
+++ app/src/main/java/com/example/ecommerce/spotsale2/SellerActivity.java
@@ -37,7 +37,7 @@ public class SellerActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager recyclerLayoutManager;
-    private ArrayList<String> categorieslist= new ArrayList<String>();
+    private ArrayList<String> categorieslist = new ArrayList<String>();
 
     private CategoryAdapter adapter;
 
@@ -54,6 +54,12 @@ public class SellerActivity extends AppCompatActivity {
         PD.setCancelable(true);
         PD.setCanceledOnTouchOutside(false);
 
+        /*    Initialize Recycler View    */
+        recyclerView = (RecyclerView) findViewById(R.id.seller_recycler);
+        recyclerLayoutManager = new GridLayoutManager(this, 1);
+
+        PD.show();
+
         /*    Get data from Database to populate Recycler View    */
         FirebaseDatabase.getInstance().getReference().child("Categories")
                 .addValueEventListener(new ValueEventListener() {
@@ -65,36 +71,26 @@ public class SellerActivity extends AppCompatActivity {
                             Log.v("category",categorieslist.toString());
                         }
 
+                        adapter = new CategoryAdapter(categorieslist, new CategoryAdapter.OnItemClickListener() {
+
+                            public void onItemClick(String category) {
+                                Toast.makeText(SellerActivity.this,category,Toast.LENGTH_LONG);
+                            }
+                        });
+                        recyclerView.setHasFixedSize(false);
+                        recyclerView.setLayoutManager(recyclerLayoutManager);
+                        recyclerView.setAdapter(adapter);
+
+                        PD.dismiss();
+
                     }
 
                     public void onCancelled( DatabaseError databaseError ) {
 
                         Toast.makeText(SellerActivity.this,"error",Toast.LENGTH_LONG).show();
                     }
-
-
-
                 });
 
-
-        /*    Initialize Recycler View    */
-        recyclerView = (RecyclerView) findViewById(R.id.seller_recycler);
-        recyclerLayoutManager = new GridLayoutManager(this, 1);
-
-        PD.show();
-
-
-        adapter = new CategoryAdapter(categorieslist, new CategoryAdapter.OnItemClickListener() {
-
-            public void onItemClick(String category) {
-                 Toast.makeText(SellerActivity.this,category,Toast.LENGTH_LONG);
-            }
-        });
-        recyclerView.setHasFixedSize(false);
-        recyclerView.setLayoutManager(recyclerLayoutManager);
-        recyclerView.setAdapter(adapter);
-
-        PD.dismiss();
     }
 
 
